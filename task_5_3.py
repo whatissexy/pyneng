751c6af1b1d850cd15381adfad4ea51d08dc6780
whatissexy <astemirulya@mail.ru>
2021-10-25 17:34:00 +0300

    Task 5.3a

--- task_5_3.py
+++ task_5_3.py
@@ -14,12 +14,21 @@ trunk_template = [
     "switchport trunk allowed vlan {}",
 ]
 
+#access_template.append("Введите номер VLAN: ")
+#trunk_template.append("Введите разрешенные VLANы: ")
 
 template = {"access": access_template, "trunk": trunk_template}
+question = {"access": "Введите номер VLAN: ", "trunk": "Введите разрешенные VLANы: "}
 
 mode = input("Введите режим работы интерфейса (access/trunk): ")
 interface = input("Введите тип и номер интерфейса: ")
-vlans = input("Введите номер влан(ов): ")
+
+vlans = input(question[mode])
+#vlans = input(template[mode][-1])
+
+#access_template.remove("Введите номер VLAN: ")
+#trunk_template.remove("Введите разрешенные VLANы: ")
 
 print(f"interface {interface}")
-print("\n".join(template[mode]).format(vlans))
+#print("\n".join(template[mode]).format(vlans))
+print('\n'.join(template[mode]).format(vlans))

7a96fd1f1db971fe8b895dbc87596edb04fc6db1
whatissexy <astemirulya@mail.ru>
2021-10-27 17:01:08 +0300

    for/while/break_2

--- check_password_with_while_break.py
+++ check_password_with_while_break.py
@@ -7,8 +7,8 @@ while True:
     if len(password) < 8:
         print('Пароль слишком короткий!')
     elif username in password:
-        print('Пароль содержит имя пользователя, это небезопасно!')
+        print('Пароль содержит имя пользователя')
     else:
         print('Пароль для пользователя {} установлен!'.format(username))
         break
-    password = input('Введите пароль еще раз: ')
+    password = input('Введите пароль для пользователя {} еще раз: '.format(username))

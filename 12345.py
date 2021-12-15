77d358a4cf0188d4e1ce2d899a1cfeb85469ea8e
whatissexy <astemirulya@mail.ru>
2021-12-15 17:33:52 +0300

    Functions

--- 12345.py
+++ 12345.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-output = "{:25} {}\n" * 5
+'''output = "{:25} {}\n" * 5
 
 with open("ospf.txt", "r") as f:
     for line in f:
@@ -13,4 +13,7 @@ with open("ospf.txt", "r") as f:
                 "Next-Hop", route[4],
                 "Last update", route[5],
                 "Outbound Interface", route[6],
-        ))
+        ))'''
+
+#a = int(input())
+#print(a, 'мин - это', a // 60, 'час', a % 60, 'мин')
